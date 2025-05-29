b64540d6f3c6907c33783995d953146a69b3ef6c
Bommner69 <vinhprop2004@gmail.com>
2025-05-29 15:37:54 +0700

    11111

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,8 @@ import App from './App';
 
 test('renders any heading text', () => {
   render(<App />);
-  const heading = screen.getByRole('heading'); // Kiểm tra thẻ <h1> bất kỳ
+  //const heading = screen.getByRole('heading'); // Kiểm tra thẻ <h1> bất kyf
+  const heading = screen.getByText(/Con Cac/i); // Kiểm tra nội dung của thẻ <h1>
+  const paragraph = screen.getByRole('paragraph'); // Kiểm tra nội dung của thẻ <p>
   expect(heading).toBeInTheDocument();
 });
