369e3326d1fd71ea88b27509fce7c3521a0c703c
Bommner69 <vinhprop2004@gmail.com>
2025-05-30 09:55:36 +0700

    updateupdate

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 function App() {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Con Cac</h1>
-      <p>Chào mừng bạn đến với ứng dụng của chúng tôi!</p>
+      <h1>Chào Các Bạn</h1>
+     
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -3,8 +3,8 @@ import App from './App';
 
 test('renders any heading text', () => {
   render(<App />);
-  //const heading = screen.getByRole('heading'); // Kiểm tra thẻ <h1> bất kyf
-  const heading = screen.getByText(/Con Cac/i); // Kiểm tra nội dung của thẻ <h1>
-  const paragraph = screen.getByRole('paragraph'); // Kiểm tra nội dung của thẻ <p>
+
+  const heading = screen.getByText(/Chào Cô/i); 
+ 
   expect(heading).toBeInTheDocument();
 });
